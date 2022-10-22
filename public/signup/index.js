ef6f5d09e4a4fa708314e257426cb0bfc31de366
ink0rr <tomflynnbeltsazar@gmail.com>
2022-10-22 15:06:08 +0700

    Update signin and signup page layout

--- public/signup/index.js
+++ public/signup/index.js
@@ -1,24 +1,35 @@
 import { Auth } from '../lib/Auth.js';
+import { setupNavbar } from '../lib/setupNavbar.js';
 
 if (Auth.currentUser) {
-  window.location.href = '/';
+  window.location.replace('/');
 }
 
+setupNavbar(document.body);
+
 const form = document.querySelector('form');
+const err = document.getElementById('err');
+const btnSubmit = document.getElementById('submit');
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const username = formData.get('username').trim();
   const password = formData.get('password').trim();
-  const confirmPassword = formData.get('confirmPassword').trim();
+  const confirmPassword = formData.get('confirm-password').trim();
 
-  if (password !== confirmPassword) return alert('Passwords do not match');
+  err.innerText = '';
+  btnSubmit.disabled = true;
 
-  const res = await Auth.signUp(username, password);
-  if (res) {
-    window.location.href = '/';
+  if (username === '' || password === '') {
+    err.innerText = 'Please fill in all fields';
+  } else if (password !== confirmPassword) {
+    err.innerText = 'Passwords do not match';
+  } else if (await Auth.signUp(username, password)) {
+    window.location.replace('/');
   } else {
-    alert('Username already exists');
+    err.innerText = 'Username is already taken';
   }
+
+  btnSubmit.disabled = false;
 });
