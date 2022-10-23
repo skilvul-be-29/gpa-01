80eb5ebbc4b10305541889423fcf41ebf05fcd1d
ink0rr <tomflynnbeltsazar@gmail.com>
2022-10-23 14:02:06 +0700

    Add timestamp formatter

--- public/thread/detail.js
+++ public/thread/detail.js
@@ -1,105 +1,86 @@
 import { Auth } from '../lib/Auth.js';
+import { createComment } from '../lib/comments.js';
+import { formatTime } from '../lib/formatTime.js';
 import { setupNavbar } from '../lib/setupNavbar.js';
 import { getThread } from '../lib/threads.js';
-import { createComment } from '../lib/comments.js';
 
 setupNavbar(document.body);
 
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
-if (!id) {
+const threadId = params.get('id');
+if (!threadId) {
   window.location.replace('/thread/');
 }
 
-const thread = await getThread(id);
+const thread = await getThread(threadId);
 document.querySelector('main').innerHTML = buildThreadCard(thread);
 
-function buildThreadCard({ createdAt, title, content, user }) {
-  const comments = thread.comments;
-
-  let commentsHtml = '';
-  comments.forEach((comment) => {
-    commentsHtml += buildCommentCard(comment);
-  });
-
-  const date = new Date(createdAt);
+function buildThreadCard({ createdAt, title, content, user: { username, avatar } }) {
+  const time = formatTime(createdAt);
+  const comments = thread.comments.map(buildCommentCard).join('');
   return `
     <article>
       <header>
         <div class="meta">
-          <img class="avatar-small" src="${user.avatar}" alt="${user.username}" />
-          <a class="text-black" href="#">${user.username}</a> ·
-          <span>${date.toDateString()}</span>
+          <img class="avatar-small" src="${avatar}" alt="${username}" />
+          <a class="text-black" href="#">${username}</a> ·
+          <span>${time}</span>
         </div>
         <h1 class="title fw-bold">${title}</h1>
       </header>
       <section class="thread-content">
         <p>${content}</p>
       </section>
-      <h2 class="fw-bold">Comments (1)</h2>
+      <h2 class="fw-bold">Comments (${thread.comments.length})</h2>
       <section class="thread-comments">
         <form class="comment">
           <div>
-            <img class="avatar" src="${user.avatar}" alt="${user.username}" />
+            <img class="avatar" src="${avatar}" alt="${username}" />
           </div>
           <div class="comment-content">
-            <textarea id="content" name="comment" placeholder="Write a comment..."></textarea>
-            <button type="submit" id="commentSubmit">Submit</button>
+            <textarea id="content" name="content" placeholder="Write a comment..."></textarea>
+            <button type="submit" id="submit" disabled>Submit</button>
           </div>
         </form>
-        <!-- Comment Template -->
-        ${commentsHtml}
+        ${comments}
       </section>
     </article>
   `;
 }
 
-function buildCommentCard({ content, createdAt, user: { username, avatar } }) {
-  const now = new Date();
-  const createdAtDate = new Date(createdAt);
-
-  const diffMs = Math.abs(createdAtDate - now);
-  const diffMinute = Math.ceil(diffMs / (1000 * 60));
-
+function buildCommentCard({ createdAt, content, user: { username, avatar } }) {
+  const time = formatTime(createdAt);
   return `
     <div class="comment">
       <div>
-        <img
-          class="avatar"
-          src="${avatar}"
-          alt="User avatar"
-        />
+        <img class="avatar" src="${avatar}" alt="${avatar}" />
       </div>
       <div class="comment-content card">
         <a href="#" class="text-black"><b>${username}</b></a>
-        <span> · ${diffMinute}m ago</span>
-        <p>
-          ${content}
-        </p>
+        <span> · ${time}</span>
+        <p>${content}</p>
       </div>
     </div>
   `;
 }
 
-document.querySelector('main').addEventListener(
-  'click',
-  async function (event) {
-    if (event.target === document.querySelector('#commentSubmit')) {
-      event.preventDefault();
+const form = document.querySelector('form');
+const btnSubmit = document.getElementById('submit');
 
-      const content = document.querySelector('#content').value;
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const content = formData.get('content').trim();
+  if (content === '') return;
 
-      if (content === '') {
-        alert("Content shouldn't be empty");
-        return;
-      }
+  btnSubmit.disabled = true;
 
-      const userId = Auth.currentUser.id;
-      const threadId = id;
+  await createComment(content, Auth.currentUser.id, threadId);
+  window.location.reload();
+});
 
-      await createComment(content, userId, threadId);
-      window.location.reload();
-    }
-  },
-  true
-);
+const inputContent = document.getElementById('content');
+
+inputContent.addEventListener('input', () => {
+  btnSubmit.disabled = inputContent.value.trim() === '';
+});
